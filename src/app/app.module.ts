443aa4ed8de56f1c7410e9a739d5bb0679d63327
sachinonam <66748064+sachinonam@users.noreply.github.com>
2020-06-12 14:28:41 +0100

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-
-
-/* Add Amplify imports */
 import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
-import Amplify from 'aws-amplify';
-import awsconfig from '../aws-exports';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
-/* Configure Amplify resources */
-Amplify.configure(awsconfig);
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [AmplifyUIAngularModule,AppRoutingModule, BrowserModule],
-  providers: [],
-  bootstrap: [AppComponent],
+declarations: [ AppComponent],
+imports: [ BrowserModule, AppRoutingModule, AmplifyUIAngularModule],
+providers: [],
+bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
